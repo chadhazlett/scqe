28f73aedd208220bad428d2b2fdaea95900b4928
Kirsten Landsiedel <kirstenlandsiedel@gmail.com>
2020-06-23 15:01:13 -0700

    Added Roxygen2 documentation (can't find it with ? though)

--- functions.R
+++ functions.R
@@ -1,6 +1,17 @@
 #build functions here
-
 #the ivreg requires the AER package!!!
+#' SCQE estimates.
+#'
+#' \code{getSCQE} returns the scqe estimates, standard deviations, and relevant confidence
+#' intervals.
+#'
+#' @param post Binary vector corresponding to T=0,1 for each observation.
+#' @param treatment Binary or continuous vector correspoding (usually) to 0,1
+#'   (no treatment or treatment) for each observation.
+#' @param outcome Continuous vector representing the outcome for each
+#'   observation.
+#' @param delta Can take either a single value or vector of possible values for
+#'   delta.
 getSCQE = function(post, treatment, outcome, delta){
     y2 = outcome - post %*% t(delta)
     r <- data.frame(term=numeric(length(delta)), estimate=numeric(length(delta)), conf.low=numeric(length(delta)),conf.high=numeric(length(delta)))
@@ -14,4 +25,5 @@ getSCQE = function(post, treatment, outcome, delta){
     }
     return(r)
   }
+
 
