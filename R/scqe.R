e8c2a05d328c32ba0f1f253a28e9fccfcbd68da8
Kirsten Landsiedel <kirstenlandsiedel@gmail.com>
2020-06-25 11:48:38 -0700

    Added package documentation searchable with: package?scqe.

---
+++ R/scqe.R
@@ -0,0 +1,15 @@
+#' scqe: Stability Controlled Quasi-Experimentation
+#'
+#' The scqe package contains several function for statistical analysis that factor in
+#' confounding variables and their impact on estimates (Hazlett, 2019).
+#'
+#' @section scqe functions:
+#' getSCQE: gets the scqe estimates, sd, and CIs
+#'
+#'@section package dependencies:
+#'AER
+#'
+#'
+#' @docType package
+#' @name scqe
+NULL
