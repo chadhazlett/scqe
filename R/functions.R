1b01887cfe968ee6d43b8139c7837463f3e51756
Chad Hazlett <chazlett@ucla.edu>
2020-06-26 22:19:41 -0700

    Adding "export".  Using "::" to access ivreg within AER.

--- R/functions.R
+++ R/functions.R
@@ -12,11 +12,12 @@
 #'   observation.
 #' @param delta Can take either a single value or vector of possible values for
 #'   delta.
+#'@export
 getSCQE = function(post, treatment, outcome, delta){
     y2 = outcome - post %*% t(delta)
     r <- data.frame(term=numeric(length(delta)), estimate=numeric(length(delta)), conf.low=numeric(length(delta)),conf.high=numeric(length(delta)))
     for (i in 1:length(delta)){
-      iv.out = summary(ivreg(y2[,i] ~ treatment | post))
+      iv.out = summary(AER::ivreg(y2[,i] ~ treatment | post))
       est = iv.out$coef["treatment",1]
       se = iv.out$coef["treatment",2]
       conf.low = est - 1.96*se
