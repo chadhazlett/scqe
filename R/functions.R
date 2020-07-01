79e2e79ff6192928277aa9f88bd835f86a0a0fab
Kirsten Landsiedel <kirstenlandsiedel@gmail.com>
2020-07-01 14:06:16 -0700

    have plot method return ggplot object (I think this is how to do this?)

--- R/functions.R
+++ R/functions.R
@@ -71,8 +71,8 @@ scqe = function(post, treatment, outcome, delta, ...){
 #' @export
 #'
 plot.scqe = function(scqe.obj){
-  ggplot2::ggplot() +
-  ggplot2::geom_pointrange(data=scqe.obj, mapping=aes(x=term, y=estimate, ymin=conf.low, ymax=conf.high),  size=.5,shape=16) +ylab("Average treatment effect on treated") +xlab("Delta") + coord_flip() + theme_bw()
+  return(ggplot2::ggplot() +
+  ggplot2::geom_pointrange(data=scqe.obj, mapping=aes(x=term, y=estimate, ymin=conf.low, ymax=conf.high),  size=.5,shape=16) +ylab("Average treatment effect on treated") +xlab("Delta") + coord_flip() + theme_bw())
   }
 
 
