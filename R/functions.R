a8116fff0be14efb236beab83a1122af4bd97ae0
Kirsten Landsiedel <kirstenlandsiedel@gmail.com>
2020-07-01 13:55:18 -0700

    fixed plot method axis labels

--- R/functions.R
+++ R/functions.R
@@ -72,7 +72,7 @@ scqe = function(post, treatment, outcome, delta, ...){
 #'
 plot.scqe = function(scqe.obj){
   ggplot2::ggplot() +
-  geom_pointrange(data=scqe.obj, mapping=aes(x=term, y=estimate, ymin=conf.low, ymax=conf.high),  size=.5,shape=16) +ylab("Estimated effect on trend's outcome, 95% CI") +xlab("Assumed outcome shift under no tmt change (delta)") + coord_flip() + theme_bw()
+  geom_pointrange(data=scqe.obj, mapping=aes(x=term, y=estimate, ymin=conf.low, ymax=conf.high),  size=.5,shape=16) +ylab("Average treatment effect on treated") +xlab("Delta") + coord_flip() + theme_bw()
   }
 
 
