696fc4cec91ceb76da86517d4879ca1b54ab5328
Kirsten Landsiedel <kirstenlandsiedel@gmail.com>
2020-07-30 14:57:55 -0700

    organized/labeled better/moved stuff around added 2Cohort, summ stats scqe fxn from shiny app added delta optim method for 2cohort sunnmary stats/full data
    SUMMARY METHOD WIP

--- R/functions.R
+++ R/functions.R
@@ -14,6 +14,8 @@
 #' @name scqe-package
 NULL
 
+## 2 COHORT CASE FULL DATA FXN
+
 #' Stability controlled quasi-experiment (scqe) for 2 cohort case FULL DATA
 #'
 #' @description
@@ -56,10 +58,21 @@ scqe = function(post, treatment, outcome, delta, ...){
     conf.high = est + 1.96*se
     r[i,] = c(delta[i], est, conf.low, conf.high)
   }
+  post <<- post
+  treatment <<- treatment
+  outcome <<- outcome
+  delta <<- delta
   class(r) <- c("scqe", "data.frame")
   return(r)
 }
 
+
+
+
+
+
+
+##ONE COHORT CASE FULL DATA FXN
 
 #' Stability controlled quasi-experiment (scqe) for 1 cohort case with FULL DATA
 #'
@@ -85,8 +98,9 @@ scqe = function(post, treatment, outcome, delta, ...){
 #'
 #'
 #' scqe_1cohort.out = scqe_1cohort(treatment=tx, outcome=y, delta=c(-0.1,0,.1))
-#'
 #' plot(scqe_1cohort.out)
+#'
+#'
 #'
 #'@export
 scqe_1cohort = function(treatment, outcome, delta){
@@ -94,7 +108,7 @@ scqe_1cohort = function(treatment, outcome, delta){
     warning("One or more delta(s) are not in range")
   }
   N <- length(treatment) #number of obs
-  pi1 <- sum(tx)/N #number of treated ind/N
+  pi1 <- sum(treatment)/N #number of treated ind/N
   Ybar_T1 <- sum(outcome)/N #the sum of outcomes for treated and untreated/N
 
   r <- data.frame(term=numeric(length(delta)), estimate=numeric(length(delta)), conf.low=numeric(length(delta)),conf.high=numeric(length(delta)))
@@ -110,14 +124,116 @@ scqe_1cohort = function(treatment, outcome, delta){
     r[i,] <- c(delta[i], Beta_SCQE_1C, Beta_SCQE_1C - 1.96*SE_B_SCQE_1C, Beta_SCQE_1C + 1.96*SE_B_SCQE_1C)
 
   }
+  treatment <<- treatment
+  outcome <<- outcome
+  delta <<- delta
   class(r) <- c("scqe", "data.frame")
   return(r)
+
 }
 
 
 
 
 
+## 2 COHORT CASE SUM STATS FXN
+# Put examples here!
+# set.seed(1234)
+# delta_range_SCQE.out <- delta_range_SCQE(200, 150, 50, 0, 20, 1, 5, 0, .1, 1)
+# plot(delta_range_SCQE.out)
+#2c summary stat case
+delta_range_SCQE <- function(untr_pre,untr_post,tr_post,tr_pre,Y_tr_post,
+                             Y_untr_post,Y_tr_pre,Y_untr_pre,
+                             min_delta, max_delta){
+
+  if(max_delta == min_delta){
+    #spread out the delta range to +/- 0.2 from the single entered
+    #delta, ensuring the range doesn't go beyond the possible bounds
+    min_delta <- max(-0.99, min_delta - 0.20)
+    max_delta <- min(0.99, min_delta + 0.40)
+    min_delta <- max_delta - 0.40
+  }
+
+  delta_list <- seq(from = max_delta, to = min_delta, length.out = 11)
+
+  N_pre <- untr_pre + tr_pre
+  N_post <- untr_post + tr_post
+  N <- N_pre + N_post
+  Y_pre <- Y_tr_pre + Y_untr_pre
+  Y_post <- Y_tr_post + Y_untr_post
+
+  P_T1 <- N_post/N
+  P_D1 <- (tr_post + tr_pre)/N
+  P_D1_T1 <- tr_post/N
+  P_D1_T0 <- tr_pre/N
+  P_D0_T1 <- (N_post - tr_post)/N
+  P_D0_T0 <- (N_pre - tr_pre)/N
+
+  P_T1_D1_Y1 <- Y_tr_post/N
+  P_T0_D1_Y1 <- Y_tr_pre/N
+  P_T1_D0_Y1 <- Y_untr_post/N
+  P_T0_D0_Y1 <- Y_untr_pre/N
+  P_T1_D1_Y0 <- (tr_post - Y_tr_post)/N
+  P_T0_D1_Y0 <- (tr_pre - Y_tr_pre)/N
+  P_T1_D0_Y0 <- (N_post - tr_post - Y_untr_post)/N
+  P_T0_D0_Y0 <- (N_pre - tr_pre - Y_untr_pre)/N
+
+  Beta_SCQE <- NULL
+  SE_B_SCQE <- NULL
+
+  for(d in delta_list){
+
+    #mean(delta_Y[post==1])
+    #mean(outcome[post==1])-delta
+    #mean(outcome&post)/mean(post) - delta
+    tildeY_in_post <- Y_post/N_post - d ###
+
+    #mean(delta_Y[post==0])
+    #mean(outcome[post==0])
+    #mean(outcome&!post)/mean(!post)
+    tildeY_in_pre <- Y_pre/N_pre
+
+    #mean(tr[post==1])
+    #mean(tr==1&post==1)/mean(post==1)
+    tr_in_post <- tr_post/N_post
+
+    #mean(tr[post==0])
+    #mean(tr==1&post==0)/mean(post==0)
+    tr_in_pre <- tr_pre/N_pre
+
+    Beta_SCQE_delta <- (tildeY_in_post - tildeY_in_pre) / (tr_in_post - tr_in_pre) ###
+
+    #mean(delta_Y)
+    #mean(outcome) - delta*mean(post)
+    tildeY_all <- (Y_pre + Y_post - d*N_post)/N ###
+
+    Beta_0 <- tildeY_all - Beta_SCQE_delta*P_D1 ###
+
+
+    SE_B_SCQE_delta <- sqrt((P_T1*(1-P_T1))/(N-2)
+                            *(P_T1_D1_Y1*(1-d-Beta_0-Beta_SCQE_delta)^2 + P_T1_D0_Y1*(1-d-Beta_0)^2 +
+                                P_T1_D1_Y0*(-d-Beta_0-Beta_SCQE_delta)^2 + P_T1_D0_Y0*(-d-Beta_0)^2 +
+                                P_T0_D1_Y1*(1-Beta_0-Beta_SCQE_delta)^2 + P_T0_D0_Y1*(1-Beta_0)^2 +
+                                P_T0_D1_Y0*(-Beta_0-Beta_SCQE_delta)^2 + P_T0_D0_Y0*(-Beta_0)^2)
+    )/
+      (P_D1_T1*(1-P_D1)*(1-P_T1) + P_D1_T0*(1-P_D1)*(-P_T1) +
+         P_D0_T1*(-P_D1)*(1-P_T1) + P_D0_T0*(-P_D1)*(-P_T1))      ###
+
+    Beta_SCQE <- c(Beta_SCQE, Beta_SCQE_delta)
+    SE_B_SCQE <- c(SE_B_SCQE, SE_B_SCQE_delta)
+  }
+  SCQE_2C_df <- data.frame(assumed_pre2post_shift = delta_list, SCQE_estimate = Beta_SCQE, SCQE_stderr = SE_B_SCQE,
+                           term = delta_list, estimate = Beta_SCQE,
+                           conf.low = Beta_SCQE - 1.96*SE_B_SCQE, conf.high = Beta_SCQE + 1.96*SE_B_SCQE)
+  class(SCQE_2C_df) <- c("scqe","data.frame")
+  return(SCQE_2C_df)
+  #return(list(delta_list = delta_list, Beta_SCQE = Beta_SCQE, SE_B_SCQE = SE_B_SCQE))
+}
+
+
+
+
+##ONE COHORT CASE SUM STATS FXN
 
 #' Stability controlled quasi-experiment (scqe) for 1 cohort case SUMMARY STATISTICS AS INPUT
 #'
@@ -184,30 +300,13 @@ one_cohort_scqe <- function(untr_1C, Y_untr_1C, tr_1C, Y_tr_1C, min_outcome, max
 
 
 
-#' Plot method for \code{scqe}
-#' @rdname plot.scqe
-#' @description
-#' The \code{print} method provides...
-#'
-#' @param scqe.obj an object of class \code{\link{scqe}}
-#'
-#' @examples
-#' # give example here
-#'
-#' @export
-#'
-plot.scqe = function(scqe.obj){
-  return(ggplot2::ggplot(scqe.obj, ggplot2::aes(x=term, y=estimate, ymin=conf.low, ymax=conf.high)) +
-           ggplot2::geom_pointrange(size=.5, shape=16) +
-           ggplot2::ylab("Average treatment effect on treated") +
-           ggplot2::xlab("Delta") + ggplot2::coord_flip() + ggplot2::theme_bw() +
-           ggplot2::geom_hline(yintercept = 0, color="gray50"))
-}
 
 
 
 
-#the below function is for the summary statistics 1c case
+
+
+#DELTA OPTIM: ONE COHORT SUMMARY STATS
 
 #' Delta optimization method for \code{scqe}
 #' @rdname delta.optim.scqe
@@ -240,7 +339,162 @@ delta.optim.scqe <- function(Y_T0, untreated, Y_untreated, treated, Y_treated, o
 
 
 
-#DELTA OPTIM FOR 1C FULL DATA CASE:
+
+
+
+#DELTA OPTIM: TWO COHORT SUMMARY STATS
+
+delta_optim_SCQE_2C <- function(delta,untr_pre,untr_post,tr_post,tr_pre,
+                                Y_tr_post, Y_untr_post,Y_tr_pre,Y_untr_pre,
+                                obj, specified = NULL){
+
+  N_pre <- untr_pre + tr_pre
+  N_post <- untr_post + tr_post
+  N <- N_pre + N_post
+  Y_pre <- Y_tr_pre + Y_untr_pre
+  Y_post <- Y_tr_post + Y_untr_post
+
+  P_T1 <- N_post/N
+  P_D1 <- (tr_post + tr_pre)/N
+  P_D1_T1 <- tr_post/N
+  P_D1_T0 <- tr_pre/N
+  P_D0_T1 <- (N_post - tr_post)/N
+  P_D0_T0 <- (N_pre - tr_pre)/N
+
+  P_T1_D1_Y1 <- Y_tr_post/N
+  P_T0_D1_Y1 <- Y_tr_pre/N
+  P_T1_D0_Y1 <- Y_untr_post/N
+  P_T0_D0_Y1 <- Y_untr_pre/N
+  P_T1_D1_Y0 <- (tr_post - Y_tr_post)/N
+  P_T0_D1_Y0 <- (tr_pre - Y_tr_pre)/N
+  P_T1_D0_Y0 <- (N_post - tr_post - Y_untr_post)/N
+  P_T0_D0_Y0 <- (N_pre - tr_pre - Y_untr_pre)/N
+
+  tildeY_in_post <- Y_post/N_post - delta
+
+  tildeY_in_pre <- Y_pre/N_pre
+
+  tr_in_post <- tr_post/N_post
+
+  tr_in_pre <- tr_pre/N_pre
+
+  Beta_SCQE_delta <- (tildeY_in_post - tildeY_in_pre) / (tr_in_post - tr_in_pre)
+
+  tildeY_all <- (Y_pre + Y_post - delta*N_post)/N
+
+  Beta_0 <- tildeY_all - Beta_SCQE_delta*P_D1
+
+  SE_B_SCQE_delta <- sqrt((P_T1*(1-P_T1))/(N-2)
+                          *(P_T1_D1_Y1*(1-delta-Beta_0-Beta_SCQE_delta)^2 + P_T1_D0_Y1*(1-delta-Beta_0)^2 +
+                              P_T1_D1_Y0*(-delta-Beta_0-Beta_SCQE_delta)^2 + P_T1_D0_Y0*(-delta-Beta_0)^2 +
+                              P_T0_D1_Y1*(1-Beta_0-Beta_SCQE_delta)^2 + P_T0_D0_Y1*(1-Beta_0)^2 +
+                              P_T0_D1_Y0*(-Beta_0-Beta_SCQE_delta)^2 + P_T0_D0_Y0*(-Beta_0)^2)
+  )/
+    (P_D1_T1*(1-P_D1)*(1-P_T1) + P_D1_T0*(1-P_D1)*(-P_T1) +
+       P_D0_T1*(-P_D1)*(1-P_T1) + P_D0_T0*(-P_D1)*(-P_T1))
+
+  if(obj == "zero"){return(Beta_SCQE_delta^2)}
+  if(obj == "spec"){return((specified - Beta_SCQE_delta)^2)}
+  #If treatment use is increasing, the threshold delta for a significant
+  #negative effect is a minimum, and the threshold delta for a significant
+  #positive effect is a maximum
+  if(P_D1_T1 > P_D1_T0){
+    if(obj == "neg"){return((Beta_SCQE_delta + 1.96*SE_B_SCQE_delta)^2)}
+    if(obj == "pos"){return((Beta_SCQE_delta - 1.96*SE_B_SCQE_delta)^2)}
+  }
+  #If treatment use is decreasing, these minimums and maximums flip
+  if(obj == "pos"){return((Beta_SCQE_delta + 1.96*SE_B_SCQE_delta)^2)}
+  if(obj == "neg"){return((Beta_SCQE_delta - 1.96*SE_B_SCQE_delta)^2)}
+}
+
+
+
+
+
+#DELTA OPTIM: TWO COHORT FULL DATA
+
+delta.optim.scqe2 <- function(post, treatment, outcome, delta, obj, specified = NULL){
+
+  untr_pre <- length(intersect(which(treatment == 0), which(post == 0)))
+  untr_post <- length(intersect(which(treatment == 0), which(post == 1)))
+  tr_post <- length(intersect(which(treatment == 1), which(post == 1)))
+  tr_pre <- length(intersect(which(treatment == 1), which(post == 0)))
+  Y_tr_post <- sum(outcome[intersect(which(treatment == 1), which(post == 1))])
+  Y_untr_post <- sum(outcome[intersect(which(treatment == 0), which(post == 1))])
+  Y_tr_pre <- sum(outcome[intersect(which(treatment == 1), which(post == 0))])
+  Y_untr_pre <- sum(outcome[intersect(which(treatment == 0), which(post == 0))])
+
+  N_pre <- untr_pre + tr_pre
+  N_post <- untr_post + tr_post
+  N <- N_pre + N_post
+  Y_pre <- Y_tr_pre + Y_untr_pre
+  Y_post <- Y_tr_post + Y_untr_post
+
+  P_T1 <- N_post/N
+  P_D1 <- (tr_post + tr_pre)/N
+  P_D1_T1 <- tr_post/N
+  P_D1_T0 <- tr_pre/N
+  P_D0_T1 <- (N_post - tr_post)/N
+  P_D0_T0 <- (N_pre - tr_pre)/N
+
+  P_T1_D1_Y1 <- Y_tr_post/N
+  P_T0_D1_Y1 <- Y_tr_pre/N
+  P_T1_D0_Y1 <- Y_untr_post/N
+  P_T0_D0_Y1 <- Y_untr_pre/N
+  P_T1_D1_Y0 <- (tr_post - Y_tr_post)/N
+  P_T0_D1_Y0 <- (tr_pre - Y_tr_pre)/N
+  P_T1_D0_Y0 <- (N_post - tr_post - Y_untr_post)/N
+  P_T0_D0_Y0 <- (N_pre - tr_pre - Y_untr_pre)/N
+
+  tildeY_in_post <- Y_post/N_post - delta
+
+  tildeY_in_pre <- Y_pre/N_pre
+
+  tr_in_post <- tr_post/N_post
+
+  tr_in_pre <- tr_pre/N_pre
+
+  Beta_SCQE_delta <- (tildeY_in_post - tildeY_in_pre) / (tr_in_post - tr_in_pre)
+
+  tildeY_all <- (Y_pre + Y_post - delta*N_post)/N
+
+  Beta_0 <- tildeY_all - Beta_SCQE_delta*P_D1
+
+  SE_B_SCQE_delta <- sqrt((P_T1*(1-P_T1))/(N-2)
+                          *(P_T1_D1_Y1*(1-delta-Beta_0-Beta_SCQE_delta)^2 + P_T1_D0_Y1*(1-delta-Beta_0)^2 +
+                              P_T1_D1_Y0*(-delta-Beta_0-Beta_SCQE_delta)^2 + P_T1_D0_Y0*(-delta-Beta_0)^2 +
+                              P_T0_D1_Y1*(1-Beta_0-Beta_SCQE_delta)^2 + P_T0_D0_Y1*(1-Beta_0)^2 +
+                              P_T0_D1_Y0*(-Beta_0-Beta_SCQE_delta)^2 + P_T0_D0_Y0*(-Beta_0)^2)
+  )/
+    (P_D1_T1*(1-P_D1)*(1-P_T1) + P_D1_T0*(1-P_D1)*(-P_T1) +
+       P_D0_T1*(-P_D1)*(1-P_T1) + P_D0_T0*(-P_D1)*(-P_T1))
+
+  if(obj == "zero"){return(Beta_SCQE_delta^2)}
+  if(obj == "spec"){return((specified - Beta_SCQE_delta)^2)}
+  #If treatment use is increasing, the threshold delta for a significant
+  #negative effect is a minimum, and the threshold delta for a significant
+  #positive effect is a maximum
+  if(P_D1_T1 > P_D1_T0){
+    if(obj == "less"){return((Beta_SCQE_delta + 1.96*SE_B_SCQE_delta)^2)}
+    if(obj == "harm"){return((Beta_SCQE_delta - 1.96*SE_B_SCQE_delta)^2)}
+  }
+  #If treatment use is decreasing, these minimums and maximums flip
+  if(obj == "less"){return((Beta_SCQE_delta + 1.96*SE_B_SCQE_delta)^2)}
+  if(obj == "harm"){return((Beta_SCQE_delta - 1.96*SE_B_SCQE_delta)^2)}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#DELTA OPTIM: ONE COHORT FULL DATA
 
 delta.optim.scqe.1cfull <- function(treatment, outcome, delta, obj, specified = NULL){
 
@@ -261,9 +515,30 @@ delta.optim.scqe.1cfull <- function(treatment, outcome, delta, obj, specified = 
 
 
 
+##SCQE CLASS PLOT METHOD
+
+#' Plot method for \code{scqe}
+#' @rdname plot.scqe
+#' @description
+#' The \code{print} method provides...
+#'
+#' @param scqe.obj an object of class \code{\link{scqe}}
+#'
+#' @examples
+#' # give example here
+#'
+#' @export
+#'
+plot.scqe = function(scqe.obj){
+  return(ggplot2::ggplot(scqe.obj, ggplot2::aes(x=term, y=estimate, ymin=conf.low, ymax=conf.high)) +
+           ggplot2::geom_pointrange(size=.5, shape=16) +
+           ggplot2::ylab("Average treatment effect on treated") +
+           ggplot2::xlab("Delta") + ggplot2::coord_flip() + ggplot2::theme_bw() +
+           ggplot2::geom_hline(yintercept = 0, color="gray50"))
+}
 
 
-
+##SCQE SUMMARY METHOD
 
 #Right now this is only working for 1C full data case example/I am having trouble making it work more generally:
 
@@ -289,7 +564,7 @@ summary.scqe = function(scqe.obj){
 
   # optimize for the "less likely case"
   opt_less_1C_full <- round(as.numeric(optimize(f = delta.optim.scqe.1cfull, interval = c(-1,1),
-             treatment=tx, outcome=y, obj = "less", tol = 0.0001)[1]), 3)
+             treatment=treatment, outcome=outcome, obj = "less", tol = 0.0001)[1]), 3)
 
   # claim: treatment makes outcome less likely
   cat("To claim the treatment made the outcome significantly less likely,\n one must claim the shift in outcomes under no treatment change was",
@@ -297,14 +572,14 @@ summary.scqe = function(scqe.obj){
 
   #optimize for "more likely case"
   opt_harm_1C_full <- round(as.numeric(optimize(f = delta.optim.scqe.1cfull, interval = c(-1,1),
-                                                treatment=tx, outcome=y, obj = "harm", tol = 0.0001)[1]), 3)
+                                                treatment=treatment, outcome=outcome,  obj = "harm", tol = 0.0001)[1]), 3)
   # claim: treatment makes outcome more likely
   cat("\n To claim the treatment made the outcome significantly more likely,\n one must claim the shift in outcomes under no treatment change was",
       opt_harm_1C_full)
 
   # optimize for the "no effect case"
   opt_zero_1C_full <- round(as.numeric(optimize(f = delta.optim.scqe.1cfull, interval = c(-1,1),
-                                                treatment=tx, outcome=y, obj = "zero", tol = 0.0001)[1]), 3)
+                                                treatment=treatment, outcome=outcome, obj = "zero", tol = 0.0001)[1]), 3)
 
   #claim: treatment had 0 effect
   cat("\n To claim the treatment had exactly 0 effect on the outcome,\n one must claim the shift in outcomes under no treatment change was exactly",
